0e214074e2d50ca673c44f68541290235459da19
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-01-19 23:42:51 +0530

    Improve codebase

--- Demo/src/SDPdemo.java
+++ Demo/src/SDPdemo.java
@@ -5,12 +5,11 @@ public class SDPdemo {
     public static void main(String[] args) {
         SmartDelimiterPredictor sdp = new SmartDelimiterPredictor();
         System.out.println(sdp.closeDelimiters("loge[sin(2+56^{58*cos(log0["));
-        System.out.println(sdp.findFunctionForParenthesisAt(19, new String[] { "loge", "(", "sin", "(", "2", "+", "56",
-                "^", "{", "58", "*", "cos", "(", "log0", "(", "23", ")", ")", "}", ")", ")" }));
-        System.out.println(sdp.findFunctionForBraceAt(17, new String[] { "loge", "{", "sin", "{", "2", "+", "56", "^",
-                "(", "58", "*", "cos", "{", "log0", "{", "23", "}", "}", "]", "}", "}" }));
-        System.out.println(sdp.findFunctionForBrackettAt(20, new String[] { "loge", "[", "sin", "[", "2", "+", "56",
-                "^", "{", "58", "*", "cos", "[", "log0", "[", "23", "]", "]", "}", "]", "]" }));
+        System.out.println(sdp.findFunctionForDelimiterAt(19, new String[] { "loge", "(", "sin", "(", "2", "+", "56",
+                "^", "{", "58", "*", "cos", "(", "log0", "(", "23", ")", ")", "}", ")", ")" }, ")"));
+        System.out.println(sdp.findFunctionForDelimiterAt(17, new String[] { "loge", "{", "sin", "{", "2", "+", "56", "^",
+                "(", "58", "*", "cos", "{", "log0", "{", "23", "}", "}", "]", "}", "}" }, "{"));
+        System.out.println(sdp.findFunctionForDelimiterAt(20, new String[] { "loge", "[", "sin", "[", "2", "+", "56",
+                "^", "{", "58", "*", "cos", "[", "log0", "[", "23", "]", "]", "}", "]", "]" }, "["));
     }
-
 }
